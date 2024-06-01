3f3b37158a32a16a1d1210e803ebfb00dc7f3b0a
Justin Shannon <justin@clowd.io>
2024-06-01 07:51:03 -0600

    Fix missing virtual

--- include/afv-native/audio/FilterSource.h
+++ include/afv-native/audio/FilterSource.h
@@ -52,7 +52,7 @@ namespace afv_native {
         class FilterSource : public ISampleSource {
         public:
             FilterSource(std::shared_ptr<ISampleSource> srcSource);
-            ~FilterSource() override;
+            virtual ~FilterSource() override;
 
             SourceStatus getAudioFrame(SampleType *bufferOut) override;
 
