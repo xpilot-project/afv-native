9956ea5b7d4b5b1e4868203b7ee711ee0ecf12c6
Pierre Ferran <pierr3@users.noreply.github.com>
2024-10-22 23:26:47 +0200

    Don't move transceivers store out of API session

--- src/afv/APISession.cpp
+++ src/afv/APISession.cpp
@@ -428,5 +428,5 @@ void APISession::_stationTransceiversCallback(http::RESTRequest *req, bool succe
 }
 
 std::map<std::string, std::vector<dto::StationTransceiver>> APISession::getStationTransceivers() const {
-    return std::move(mStationTransceivers);
+    return mStationTransceivers;
 }
